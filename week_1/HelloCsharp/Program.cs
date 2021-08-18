c1e1df34e0e7de5f16a547df4eb1e6adc0e0dd67
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-18 11:57:40 -0400

    first c# code

---
+++ week_1/HelloCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloCsharp
+{
+  class Program
+  {
+    //Build a simple calculator using 5 methods: Add, Multiply, Subtract, Divide, Print
+
+
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.ReadLine();
+    }
+  }
+}
