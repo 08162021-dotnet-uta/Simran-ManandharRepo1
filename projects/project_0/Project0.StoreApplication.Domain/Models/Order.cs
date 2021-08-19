fbf513a9449aeef19fcd14015f039e33d75a41fd
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-19 19:57:36 -0400

    class codes

---
+++ projects/project_0/Project0.StoreApplication.Domain/Models/Order.cs
@@ -0,0 +1,11 @@
+namespace Project0.StoreApplication.Domain.Models
+{
+  class Order
+  {
+    // public Customer Customer { }
+
+    // public Store Store { }
+
+    // public Product Product { }
+  }
+}
--- projects/project_0/Project0.StoreApplication.Storage/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Project0.StoreApplication.Storage
-{
-    public class Class1
-    {
-    }
-}
