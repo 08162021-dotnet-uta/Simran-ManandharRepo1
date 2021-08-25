58379f71366d6f66d957cc40a4e797b1d70533ef
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-25 19:58:22 -0400

    create file adapter class for reading and writing data

---
+++ projects/project_0/Project0.StoreApplication.Storage/Adapters/FileAdapter.cs
@@ -0,0 +1,34 @@
+namespace Project0.StoreApplication.Storage.Adapters
+{
+  public class Adapters
+  {
+    public void ReadFromFile()
+    {
+      var path = "~/revature/simran_code/data/project_0.xml";
+
+      var file = new StreamReader(path);
+
+      var xml = new XmlSerializer(typeof(List<Store>));
+
+      var stores = xml.Deserialize(file) as List<Store>;
+
+      file.Close();
+
+      return stores;
+    }
+
+    public void WriteToFile(List<Store> stores)
+    {
+      var path = "~/revature/simran_code/data/project_0.xml";
+
+      var file = new StreamWriter(path);
+
+      var xml = new XmlSerializer(typeof(List<Store>));
+
+      xml.Serialize(file, stores);
+
+      file.Close();
+
+    }
+  }
+}
