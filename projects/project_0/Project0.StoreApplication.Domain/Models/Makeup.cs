cd56c628fa645470d6bd17d302936d7d50ca47be
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-25 19:11:35 -0400

    create makeup store class

---
+++ projects/project_0/Project0.StoreApplication.Domain/Models/Makeup.cs
@@ -0,0 +1,12 @@
+using Project0.StoreApplication.Domain.Abstracts;
+
+namespace Project0.StoreApplication.Domain.Models
+{
+  public class Makeup : Store
+  {
+    public Makeup()
+    {
+      Name = "Makeup Store";
+    }
+  }
+}
