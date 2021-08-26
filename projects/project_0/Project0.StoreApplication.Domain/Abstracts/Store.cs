9be2e04734e5c88a891421ea736f2e0246659121
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-25 21:18:58 -0400

    add xml include for adding data

--- projects/project_0/Project0.StoreApplication.Domain/Abstracts/Store.cs
+++ projects/project_0/Project0.StoreApplication.Domain/Abstracts/Store.cs
@@ -1,8 +1,13 @@
+using System.Xml.Serialization;
+using Project0.StoreApplication.Domain.Models;
+
 namespace Project0.StoreApplication.Domain.Abstracts
 {
+  [XmlInclude(typeof(Makeup))]
+
   public abstract class Store
   {
-    public string Name { get; protected set; }
+    public string Name { get; set; }
 
     public override string ToString()
     {
