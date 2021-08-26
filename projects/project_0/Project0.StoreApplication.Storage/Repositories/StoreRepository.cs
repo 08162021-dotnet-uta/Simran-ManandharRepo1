2c8cc18d256639af62a415ae7a42ff1e646f88ab
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-25 20:12:28 -0400

    instantiated file adapter to write the stores in xml data

--- projects/project_0/Project0.StoreApplication.Storage/Repositories/StoreRepository.cs
+++ projects/project_0/Project0.StoreApplication.Storage/Repositories/StoreRepository.cs
@@ -1,20 +1,37 @@
+using System;
 using System.Collections.Generic;
+using Project0.StoreApplication.Domain.Abstracts;
 using Project0.StoreApplication.Domain.Models;
 
 namespace Project0.StoreApplication.Storage.Repositories
 {
   public class StoreRepository
   {
-    public List<Store> Stores { get; set; }
+    public List<Store> Stores { get; }
 
     public StoreRepository()
     {
-      Stores = new List<Store>()
+
+      var fileAdapter = new FileAdapter();
+
+      fileAdapter.WriteToFile(new List<Store>()
       {
-        new Store(){Name = "Store001"},
-        new Store(){Name = "Store002"},
-        new Store(){Name = "Store003"}
-      };
+        new Makeup(), //sores.Add(new Store());
+        new Makeup(),
+        new Makeup()
+      });
+      
+      // if(fileAdapter.ReadFromFile() == null)
+      // {
+      //   fileAdapter.WriteToFile(new List<Store>()
+      // {
+      //   new Makeup(), //sores.Add(new Store());
+      //   new Makeup(),
+      //   new Makeup()
+      // });
+      // }
+      
+      // Stores = fileAdapter.ReadFromFile();
     }
 
   }
