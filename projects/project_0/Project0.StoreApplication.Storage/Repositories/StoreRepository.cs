7e7413ce8f864dc8d7387501bebca87203f14db8
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-25 21:06:27 -0400

    deleted readfromfile section

--- projects/project_0/Project0.StoreApplication.Storage/Repositories/StoreRepository.cs
+++ projects/project_0/Project0.StoreApplication.Storage/Repositories/StoreRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Project0.StoreApplication.Domain.Abstracts;
 using Project0.StoreApplication.Domain.Models;
+using Project0.StoreApplication.Storage.Adapters;
 
 namespace Project0.StoreApplication.Storage.Repositories
 {
@@ -21,17 +22,6 @@ namespace Project0.StoreApplication.Storage.Repositories
         new Makeup()
       });
       
-      // if(fileAdapter.ReadFromFile() == null)
-      // {
-      //   fileAdapter.WriteToFile(new List<Store>()
-      // {
-      //   new Makeup(), //sores.Add(new Store());
-      //   new Makeup(),
-      //   new Makeup()
-      // });
-      // }
-      
-      // Stores = fileAdapter.ReadFromFile();
     }
 
   }
