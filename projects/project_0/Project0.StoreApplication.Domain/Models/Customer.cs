53115728745156154e40666e58bfd3738228f154
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-28 13:38:10 -0400

    add a customer

--- projects/project_0/Project0.StoreApplication.Domain/Models/Customer.cs
+++ projects/project_0/Project0.StoreApplication.Domain/Models/Customer.cs
@@ -1,7 +1,22 @@
+using System.Collections.Generic;
+
 namespace Project0.StoreApplication.Domain.Models
 {
   public class Customer
   {
+    public string Name { get; set; }
+
+    public List<Order> Orders { get; set; }
+
+    public override string ToString()
+    {
+      return $"{Name} with {Orders.Count} orders so far";
+    }
+
+    public Customer()
+    {
+      Name = "Simran";
+    }
 
   }
 }
