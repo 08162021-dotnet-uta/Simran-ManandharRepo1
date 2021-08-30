fd7ce758698982235520b7054749ac55f4f3ccf0
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-29 22:47:59 -0400

    add order to store model

--- projects/project_0/Project0.StoreApplication.Domain/Models/Store.cs
+++ projects/project_0/Project0.StoreApplication.Domain/Models/Store.cs
@@ -1,12 +1,12 @@
-using System.Xml.Serialization;
-using Project0.StoreApplication.Domain.Models;
+using System.Collections.Generic;
 
-namespace Project0.StoreApplication.Domain.Abstracts
+namespace Project0.StoreApplication.Domain.Models
 {
 
   public class Store
   {
     public string Location { get; set; }
+    private List<Order> Orders = new List<Order>();
 
     public override string ToString()
     {
