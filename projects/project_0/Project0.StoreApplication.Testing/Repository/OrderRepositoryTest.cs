5710081dd66d3dfda9a763a9bc3eeb4d41ed7450
Simran MAnandhar <simran.manandhar@revature.net>
2021-08-31 11:44:39 -0400

    add test for order repo

---
+++ projects/project_0/Project0.StoreApplication.Testing/Repository/OrderRepositoryTest.cs
@@ -0,0 +1,22 @@
+using Xunit;
+using Project0.StoreApplication.Storage.Repositories;
+
+namespace Project0.OrderApplication.Testing.Repository
+{
+  public class OrderRepositoryTests
+  {
+    [Fact]
+    public void Test_OrderCollection()
+    {
+      //arrange = instance of the entity to test
+      var sut = new OrderRepository();
+
+      //act = execute sut for data
+      var actual = sut.Select();
+
+      //assert
+      Assert.NotNull(actual);
+
+    }
+  }
+}
